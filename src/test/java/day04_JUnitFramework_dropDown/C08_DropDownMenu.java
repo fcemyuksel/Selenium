900bbc96328063140864d2081d91f40282ae1092
Cem <122365538+fcemyuksel@users.noreply.github.com>
2023-10-10 21:31:31 +0300

    dropdown menu

--- src/test/java/day04_JUnitFramework_dropDown/C08_DropDownMenu.java
+++ src/test/java/day04_JUnitFramework_dropDown/C08_DropDownMenu.java
@@ -1,5 +1,6 @@
 package day04_JUnitFramework_dropDown;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
@@ -12,31 +13,35 @@ import java.util.List;
 
 public class C08_DropDownMenu extends TestBase {
     @Test
-    public <List> void test01() throws InterruptedException {
+    public void test01() throws InterruptedException {
         //● https://the-internet.herokuapp.com/dropdown adresine gidin.
         driver.get("https://the-internet.herokuapp.com/dropdown");
         //dropDown menude islem yapabilmek icin
         WebElement ddm=driver.findElement(By.xpath("(//select[@id='dropdown'])"));
         Select select=new Select(ddm);
         //	1.Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+        //select.selectByVisibleText("Option 1");
         select.selectByIndex(1);
+        //select.selectByValue("1");
         System.out.println(select.getFirstSelectedOption().getText());
         //	2.Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
         select.selectByValue("2");
         System.out.println(select.getFirstSelectedOption().getText());
         //	3.Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         select.selectByVisibleText("Option 1");
         System.out.println(select.getFirstSelectedOption().getText());
         //	4.Tüm dropdown değerleri(value) yazdırın
-        java.util.List<WebElement> optionList=select.getOptions();
-        for (WebElement each:optionList){
+        System.out.println("===================");
+        List<WebElement> optionsList=select.getOptions();
+        for (WebElement each:optionsList){
             System.out.println(each.getText());
         }
-        //	5. Dropdown’un boyutunun 4 olduğunu test edin
+        //	5. Dropdown’un boyutunun 4 olduğunu test edin
         int expectedListSize=4;
-        int actulaListSize;
-        actulaListSize = optionList.size();
+        int actulaListSize= optionsList.size();
+
+        Assert.assertEquals(expectedListSize,actulaListSize);
 
         Thread.sleep(2000);
 
